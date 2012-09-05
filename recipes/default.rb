eb0500b0d7aa78eb941f46bae52aae5461402a9b
Jeff Eklund <jeff.eklund@klarna.com>
2012-09-05 17:05:29 +0200

    recipes/default: Add -L to curl to follow 302 moved.

--- recipes/default.rb
+++ recipes/default.rb
@@ -59,7 +59,7 @@ bash "install_npm" do
   user "root"
     cwd "/tmp/"
     code <<-EOH
-    curl http://npmjs.org/install.sh | clean=no sh
+    curl -L http://npmjs.org/install.sh | clean=no sh
     EOH
 end
 
